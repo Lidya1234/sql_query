4d7297df76955aea9c82df05f3ecd39682dc43a9
Lidya1234 <lidya94gher@gmail.com>
2020-10-26 17:23:18 +0300

    Add content to sql query 6

---
+++ sql_query6.sql
@@ -0,0 +1,13 @@
+--Show the total population of the world.
+SELECT SUM(population)
+FROM world
+--List all the continents - just once each
+select DISTINCT continent from world
+--Give the total GDP of Africa
+select sum(gdp) from world where continent='Africa'
+--How many countries have an area of at least 1000000
+select count(name) from world where area >= 1000000
+--What is the total population of ('Estonia', 'Latvia', 'Lithuania')
+select sum(population) from world where name IN ('Estonia', 'Latvia', 'Lithuania')
+--For each continent show the continent and number of countries.
+select continent,count(name) from world GROUP BY continent
