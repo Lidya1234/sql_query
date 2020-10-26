10968c1ed6384f98f5db6c636d41dcb00bb03789
Lidya1234 <lidya94gher@gmail.com>
2020-10-26 12:55:08 +0300

    add sql2 file

--- sql_query1.sql
+++ sql_query1.sql
@@ -1,6 +1,7 @@
+--Show the population of Germany.
 SELECT population FROM world WHERE name='Germany';
-SELECT name ,populaTion FROM world WHERE name IN ('Brazil', 'Russia','India', 'China') ;
+--Show the name and the population for 'Sweden', 'Norway' and 'Denmark'.
+SELECT name ,population FROM world WHERE name IN ('Sweden', 'Norway','Denmark') ;
+--Show the country and the area for countries with an area between 200,000 and 250,000.
 SELECT name  FROM world WHERE  population BETWEEN 200,000 AND 250,000 ;
-SELECT name ,population FROM  world WHERE polulation BETWEEN 1,000,000 AND 1,250,000;
-SELECT name ,population FROM world WHERE name LIKE 'Al%';
-SELECT name FROM world WHERE name LIKE '%a' name LIKE '%l';
+
