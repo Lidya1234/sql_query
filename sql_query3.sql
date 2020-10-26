2eddc689eabccab9dcc004ae8c233a5f14c450a9
Lidya1234 <lidya94gher@gmail.com>
2020-10-26 13:46:03 +0300

    Add content to sql_query3

--- sql_query3.sql
+++ sql_query3.sql
@@ -10,10 +10,18 @@ select name ,population/1000000 from world where continent='south America'
 select name, population from world where name IN ('France', 'Germany','Italy')
 --Show the countries which have a name that includes the word 'United'
 select name from world where name LIKE '%United%'
-SELECT name ,population FROM  world WHERE polulation BETWEEN 1,000,000 AND 1,250,000;
-SELECT name ,population FROM world WHERE name LIKE 'Al%';
-SELECT name FROM world WHERE name LIKE '%a' name LIKE '%l';
-SELECT name ,length(name) FROM world WHERE length(name)=5 and region='EUROPE';
-SELECT name ,area*2 FROM world WHERE population=64000;
-SELECT name ,area,population FROM world WHERE area >5000 and population < 10,000,000;
-SELECT name ,population/area FROM world WHERE name IN ('china',)
+--Show the countries that are big by area or big by population. Show name, population and area.
+select name, population, area from world where population >250000000 OR area >3000000
+--Show the countries that are big by area (more than 3 million) or big by population (more than 250 million) but not both. Show name, population and area.
+select name,population,area from world where population >250000000 XOR area > 3000000
+--Show the name and population in millions and the GDP in billions for the countries of the continent 'South America'. Use the ROUND function to show the values to two decimal places.
+select name,ROUND(population/1000000,2), ROUND(gdp/1000000000,2) from  world where continent ='south America'
+--Show the name and per-capita GDP for those countries with a GDP of at least one trillion (1000000000000; that is 12 zeros). Round this value to the nearest 1000
+select name,ROUND(gdp/population,-3) FROM world WHERE gdp >= 1000000000000
+--Show the name and capital where the name and the capital have the same number of characters.
+SELECT name ,capital FROM world WHERE LENGTH(name) = LENGTH(capital)
+--Show the name and the capital where the first letters of each match. Don't include countries where the name and the capital are the same word
+SELECT name, capital FROM world where LEFT(name,1) = LEFT(capital ,1) AND capital <> name
+--Find the country that has all the vowels and no spaces in its name
+select name from world where name LIKE '%a%' AND name LIKE '%e%' AND name LIKE '%i%' AND name LIKE '%o%' AND name LIKE '%u%' AND name NOT LIKE '% %'
+
