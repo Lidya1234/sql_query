8ab03f720fe1d430df637ebbb302d6613b5caa13
Lidya1234 <lidya94gher@gmail.com>
2020-10-26 18:26:22 +0300

    Add query to sql 7[C

--- sql_query7.sql
+++ sql_query7.sql
@@ -7,4 +7,13 @@ SELECT id,stadium,team1,team2
 --Show the player, teamid, stadium and mdate for every German goal. 
 SELECT player,teamid,stadium,mdate
   FROM game JOIN goal ON (id=matchid) where teamid='GER'
---
+--Show the team1, team2 and player for every goal scored by a player called Mario player LIKE 'Mario%'
+select team1,team2,player from game JOIN goal ON (id=matchid) where player LIKE 'Mario%'
+--Show player, teamid, coach, gtime for all goals scored in the first 10 minutes gtime<=10
+SELECT player, teamid, coach, gtime
+  FROM goal JOIN eteam ON (teamid=id) 
+ WHERE gtime<=10
+--List the dates of the matches and the name of the team in which 'Fernando Santos' was the team1 coach.
+select mdate,teamname from game JOIN eteam ON (team1 = eteam.id) where coach='Fernando Santos'
+--List the player for every goal scored in a game where the stadium was 'National Stadium, Warsaw'
+select player from goal JOIN game ON (id=matchid) where stadium='National Stadium, Warsaw'
