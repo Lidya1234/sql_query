6980953b7043f1d30e37a6a592d35f3ac26a4aba
Lidya1234 <lidya94gher@gmail.com>
2020-10-27 16:24:27 +0300

    Add query 8 9 and 10 to sql query9

--- sql_query9.sql
+++ sql_query9.sql
@@ -14,3 +14,21 @@ select name , COALESCE(mobile,'07986 444 2266')  from teacher
 select teacher.name,COALESCE(dept.name,'None') from teacher LEFT JOIN dept ON dept=dept.id
 --Use COUNT to show the number of teachers and the number of mobile phones.
 select COUNT(name), COUNT(mobile) from teacher 
+--Use COUNT and GROUP BY dept.name to show each department and the number of staff. Use a RIGHT JOIN to ensure that the Engineering department is listed.
+select dept.name,COUNT(teacher.dept) from teacher RIGHT JOIN dept ON dept=dept.id GROUP BY dept.name
+--Use CASE to show the name of each teacher followed by 'Sci' if the teacher is in dept 1 or 2 and 'Art' otherwise.
+select teacher.name, 
+CASE WHEN dept=1 OR dept=2 
+     THEN 'Sci'
+      ELSE 'Art'
+END
+from teacher LEFT JOIN dept ON teacher.id =dept.id
+--Use CASE to show the name of each teacher followed by 'Sci' if the teacher is in dept 1 or 2, show 'Art' if the teacher's dept is 3 and 'None' otherwise.
+select teacher.name,
+CASE WHEN dept=1 OR dept=2
+     THEN 'Sci'
+     WHEN dept=3
+     THEN 'Art'
+     ELSE 'None'
+END 
+from teacher
